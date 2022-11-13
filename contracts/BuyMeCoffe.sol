3c66136562edd680e049bbaa865cbf9f7fce588f
Yakoub <81873091+yakoub-smartinia@users.noreply.github.com>
2022-11-13 21:03:59 +0100

    fix: fix typo 🐛

--- contracts/BuyMeCoffe.sol
+++ contracts/BuyMeCoffe.sol
@@ -29,36 +29,29 @@ contract BuyMeACoffee {
         owner = payable(msg.sender);
     }
 
-
     /**
      * @dev buy a coffee for the contract owner
      * @param _name name of the coffee buyer
      * @param _message message from the coffee buyer
-     * 
+     *
      */
-    function buyCoffe(string memory _name, string memory _message) public payable {
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
         // The sent amount must be > O ETH
         require(msg.value > 0, "Can't buy coffee with 0 ETH :( ");
 
         // Save the memo
-        memos.push(Memo(
-            msg.sender,
-            block.timestamp,
-            _name,
-            _message
-        ));
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
 
         // Emit a log event when a new memo is created!
-        emit NewMemo(msg.sender,
-            block.timestamp,
-            _name,
-            _message);
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
     }
-
 
     /**
      * @dev send the entire balance stored in this contract to the contract owner
-     * 
+     *
      */
     function withdraw() public {
         // revert if anything goes wrong.
@@ -67,10 +60,10 @@ contract BuyMeACoffee {
 
     /**
      * @dev retrieve all the memos stored on the blockchain
-     * 
+     *
      * @return - Memo's List
      */
-    function getMemos() public view returns(Memo[] memory) {
+    function getMemos() public view returns (Memo[] memory) {
         return memos;
     }
 }
