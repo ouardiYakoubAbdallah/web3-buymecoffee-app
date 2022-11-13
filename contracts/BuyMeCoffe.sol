5b7f39e10d228a1e212e94df3c33c6e1e4e3ba12
Yakoub <81873091+yakoub-smartinia@users.noreply.github.com>
2022-11-13 14:30:49 +0100

    feat: build the BuyMeCoffee contract ✨

---
+++ contracts/BuyMeCoffe.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // List of all memos received from freinds.
+    Memo[] memos;
+
+    // Address of the contract deployer
+    address payable owner;
+
+    // Deploy logic.
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+
+    /**
+     * @dev buy a coffee for the contract owner
+     * @param _name name of the coffee buyer
+     * @param _message message from the coffee buyer
+     * 
+     */
+    function buyCoffe(string memory _name, string memory _message) public payable {
+        // The sent amount must be > O ETH
+        require(msg.value > 0, "Can't buy coffee with 0 ETH :( ");
+
+        // Save the memo
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        // Emit a log event when a new memo is created!
+        emit NewMemo(msg.sender,
+            block.timestamp,
+            _name,
+            _message);
+    }
+
+
+    /**
+     * @dev send the entire balance stored in this contract to the contract owner
+     * 
+     */
+    function withdraw() public {
+        // revert if anything goes wrong.
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrieve all the memos stored on the blockchain
+     * 
+     * @return - Memo's List
+     */
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
+}
